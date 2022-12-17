2d4ac5e2078d20534ae676b72770f7ea35b9d397
michaelaboah <maboah@PlexusParc.com>
2022-12-17 14:58:30 -0500

    cleaned up Sheets page added Sheets.utils.ts next: configure Select Dropdown

--- src/pages/Sheets.svelte
+++ src/pages/Sheets.svelte
@@ -3,63 +3,55 @@
   import { defineCustomElements } from '@revolist/revogrid/loader';
   import { ioList } from '../stores/ProjectStore';
   import { persist } from '../stores/renderStore';
-  import { headerGrab } from './Sheets.utils.jsx';
+  import { columns } from './Sheets.utils';
+  import { Group, Title } from '@svelteuidev/core';
   defineCustomElements();
-  const columns = [
-    {
-      name: 'Channel #: ',
-      prop: 'channel',
-      size: 140,
-      sortable: true,
-    },
-    {
-      name: 'Input Device: ',
-      prop: 'input_device',
-      size: 150,
-      autoSize: true,
-
-      //Changes the cursor to indicate that this column can be moved.
-      headerGrab,
-      //     'div',
-      //     {
-      //       style: {
-      //         cursor: 'grab',
-      //       },
-      //     },
-      //     column.name
-      //   );
-      // },
-    },
-    {
-      name: 'Source Description: ',
-      prop: 'input_description',
-      size: 200,
-      headerGrab,
-    },
-    {
-      name: 'Note: ',
-      prop: 'note',
-      size: 200,
-    },
-  ];
 </script>
 
-<div class="revo-grid">
-  <RevoGrid
-    source="{$ioList.input_list}"
-    columns="{columns}"
-    resize
-    theme="{$persist.darkMode ? 'darkMaterial' : 'material'}"
-    range
-    useClipboard
-    filter
-    canMoveColumns
-    autoSizeColumn="{{ allColumns: true, mode: 'autoSizeAll' }}"
-  />
-</div>
+<Group direction="row" position="center" grow>
+  <div class="revo-grid">
+    <Title align="center">Input List</Title>
+    <RevoGrid
+      resize="{false}"
+      autoSizeColumn
+      range
+      useClipboard
+      filter
+      canMoveColumns
+      source="{$ioList.input_list}"
+      columns="{columns}"
+      theme="{$persist.darkMode ? 'darkMaterial' : 'material'}"
+    />
+  </div>
+  <div class="revo-grid">
+    <Title align="center">Output List</Title>
+    <RevoGrid
+      resize="{false}"
+      autoSizeColumn
+      range
+      useClipboard
+      filter
+      canMoveColumns
+      source="{$ioList.input_list}"
+      columns="{columns}"
+      theme="{$persist.darkMode ? 'darkMaterial' : 'material'}"
+    />
+  </div>
+</Group>
 
 <style>
   .revo-grid {
     height: 88vh;
+    padding-top: 1vh;
+    padding-left: 1vh;
+    padding-bottom: 3vh;
+    /* max-width: 50vw; */
+    border: 1px;
+    border-style: solid;
+    border-color: grey;
+  }
+
+  .revo-grid::-webkit-scrollbar {
+    display: none;
   }
 </style>
