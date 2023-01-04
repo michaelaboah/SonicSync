15b1786c911e34978a0bb66a11fbde82cf13c993
michaelaboah <maboah@PlexusParc.com>
2023-01-04 11:59:53 -0500

    progress on db path creation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,12 +2,14 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
+use std::{error, fs, io, path::PathBuf};
+
 use essentials::{
     communication::commands::{greet, open_project, save_as_file},
     menu::{menu_bar, menu_events},
 };
 
-use tauri;
+use tauri::{self, api::path::data_dir};
 use tauri_plugin_persisted_scope;
 use tauri_plugin_store::PluginBuilder;
 use tauri_plugin_window_state::Builder;
@@ -21,14 +23,8 @@ fn main() {
 
     tauri::Builder::default()
         .setup(|app| {
-            // let db_path = app
-            //     .path_resolver()
-            //     .resolve_resource("./sqlite-internal.db")
-            //     .unwrap().to_str().unwrap();
+            setup_database(app)?;
 
-            let test = app.path_resolver().app_data_dir().unwrap();
-
-            println!("{}", test.to_str().unwrap());
             Ok(())
         })
         .plugin(PluginBuilder::default().build())
@@ -39,4 +35,58 @@ fn main() {
         .invoke_handler(tauri::generate_handler![greet, save_as_file, open_project])
         .run(ctx)
         .expect("error while running tauri application");
+}
+
+fn setup_database(app: &mut tauri::App) -> Result<(), Box<dyn error::Error>> {
+    find_resource("resources/sqlite-internal.db", app);
+
+    let db_path: Result<PathBuf, io::Error> = app
+        .path_resolver()
+        .resolve_resource("resources/sqlite-internal.db")
+        .map(|path| match path.try_exists() {
+            Ok(true) => Ok(path),
+            Ok(false) => Ok(path),
+            Err(path_err) => Err(path_err),
+        })
+        .unwrap();
+
+    let data_dir = app
+        .path_resolver()
+        .app_data_dir()
+        .map(|mut path| match path.try_exists() {
+            Ok(false) => {
+                path.push("sqlite-internal.db");
+                println!("Full Path {}", path.to_str().unwrap());
+                Ok(path)
+            }
+            Ok(true) => {
+                path.push("sqlite-internal.db");
+                println!("Full Path {}", path.to_str().unwrap());
+                Ok(path)
+            }
+            Err(path_err) => Err(path_err),
+        })
+        .unwrap();
+    fs::copy(db_path?, data_dir?).expect("Copy Error");
+    Ok(())
+}
+
+fn find_resource(resource_path: &str, app: &mut tauri::App) -> Result<PathBuf, io::Error> {
+    match app.path_resolver().resolve_resource(resource_path) {
+        Some(path) => match path.try_exists() {
+            Ok(true) => {
+                println!("Path does exist: {}", path.to_str().unwrap());
+                Ok(path)
+            }
+            Ok(false) => {
+                println!("Path doesn't exist: {}", path.to_str().unwrap());
+                Ok(path)
+            }
+            Err(path_err) => Err(path_err),
+        },
+        None => Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "Resource Not Found!: path = resources/sqlite-internal.db",
+        )),
+    }
 }
