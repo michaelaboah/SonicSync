c2fcb6866aeced54e212724e86f7a917bbc3fe5a
michaelaboah <maboah@PlexusParc.com>
2023-01-20 23:59:27 -0500

    issue with sqlite_database in build. Likely two Tokio::main runtimes. May look into multiple threads

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,11 +3,12 @@
     windows_subsystem = "windows"
 )]
 
-use dotenvy;
+// use dotenvy;
 use essentials::{
     communication::commands::{greet, open_project, save_as_file},
     menu::{menu_bar, menu_events},
 };
+// #[cfg(test)]
 use sqlite_database::{queries::deletions::*, queries::insertions::*, queries::search::*};
 use std::env;
 use tauri;
@@ -17,15 +18,15 @@ use tauri_plugin_window_state::Builder;
 mod essentials;
 
 fn main() {
-    dotenvy::dotenv().unwrap();
-    let db_path: String = env::var("DATABASE_URL").unwrap();
+    // dotenvy::dotenv().unwrap();
+    // let db_path: String = env::var("DATABASE_URL").unwrap();
     let ctx = tauri::generate_context!();
     let menu = menu_bar::generate_menu_bar(&ctx.package_info().name);
-    let pool = sqlite_database::database_setup::initialize_db(&db_path)
+    let pool = sqlite_database::database_setup::initialize_db("sqlite:/Users/michaelaboah/Documents/Programming/SoundTools/Deadalus-Tauri/src-tauri/resources/sqlite-internal.db")
         .expect("failed to initialize database");
 
     tauri::Builder::default()
-        .manage(pool)
+        // .manage(pool)
         .plugin(PluginBuilder::default().build())
         .plugin(tauri_plugin_persisted_scope::init())
         .plugin(Builder::default().build())
@@ -35,14 +36,14 @@ fn main() {
             greet,
             save_as_file,
             open_project,
-            find_single_item,
-            find_all_items,
-            fuzzy_find_single_item,
-            find_similar_item,
-            insert_single_item,
-            delete_all_items,
-            delete_single_item,
-            fuzzy_delete_single_item,
+            // find_single_item,
+            // find_all_items,
+            // fuzzy_find_single_item,
+            // find_similar_item,
+            // insert_single_item,
+            // delete_all_items,
+            // delete_single_item,
+            // fuzzy_delete_single_item,
         ])
         .run(ctx)
         .expect("error while running tauri application");
