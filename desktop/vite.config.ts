3108ac2e5bc582a537316c0d0813e4c822ddc364
michaelaboah <michaelaboah@gmail.com>
2023-08-07 05:39:18 -0400

    "breaking houdini"

--- desktop/vite.config.ts
+++ desktop/vite.config.ts
@@ -1,9 +1,8 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import houdini from 'houdini/vite'
 import Icons from 'unplugin-icons/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit(), 
-    Icons({ compiler: "svelte"})
-  ]
+	plugins: [ /*Icons({compiler: "svelte"}) */, houdini(), sveltekit(), ]
 });
