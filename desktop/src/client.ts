8b8b7a997d29b149acae302daa75f8e97973e9d7
michaelaboah <michaelaboah@gmail.com>
2023-08-14 19:29:09 -0400

    "work on client side sesssion management later"

--- desktop/src/client.ts
+++ desktop/src/client.ts
@@ -6,11 +6,11 @@ export default new HoudiniClient({
       // "url": "http://localhost:8080/graphql"
     // uncomment this to configure the network call (for things like authentication)
     // for more information, please visit here: https://www.houdinigraphql.com/guides/authentication
-    // fetchParams({ session }) { 
-    //     return { 
-    //         headers: {
-    //             Authentication: `Bearer ${session.token}`,
-    //         }
-    //     }
-    // 
+    fetchParams({ session }: any) {
+        return {
+            headers: {
+                Authorization: `Bearer ${session}`,
+            },
+        }
+    },
 })
